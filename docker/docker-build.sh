fb3026ab8fee2b7f3f2565ce4835bd04a8633d48
Knu334 <who.is.fuu@gmail.com>
2024-11-26 02:58:13 +0900

    環境変数ファイル作成タイミング変更

--- docker/docker-build.sh
+++ docker/docker-build.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
+if [ ! -e .env ]; then
+    echo USERNAME= >> .env
+    echo PASSWORD= >> .env
+    echo NODE_ENV=production >> .env
+    echo TZ=Asia/Tokyo >> .env
+    echo LOG_LEVEL=INFO >> .env
+fi
 docker build . -f ./docker/Dockerfile -t Knu334/mydns:latest --build-arg ARCH=
--- docker/docker-run.sh
+++ docker/docker-run.sh
@@ -1,9 +1,2 @@
 #!/bin/bash
-if [ ! -e .env ]; then
-    echo USERNAME= >> .env
-    echo PASSWORD= >> .env
-    echo NODE_ENV=production >> .env
-    echo TZ=Asia/Tokyo >> .env
-    echo LOG_LEVEL=INFO >> .env
-fi
 docker run --name mydns --env-file .env --mount type=bind,src=./logs/,dst=/app/logs/ --init --rm Knu334/mydns:latest
