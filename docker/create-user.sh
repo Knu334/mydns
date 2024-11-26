6ce5a4ebfc91afe9ec236f3022f240bd00ddf8de
Knu334 <who.is.fuu@gmail.com>
2024-11-26 13:07:10 +0900

    確認用コマンドの削除

--- docker/create-user.sh
+++ docker/create-user.sh
@@ -4,7 +4,4 @@ GROUPID=${GROUP_ID}
 echo "Create User = $USERID. Group = $GROUPID"
 groupadd -g $GROUPID dockeruser
 useradd -m -s /bin/bash -u $USERID -g $GROUPID dockeruser
-whoami
-id
-ls -la /app/logs
 exec /usr/sbin/gosu $USERID "$@"
