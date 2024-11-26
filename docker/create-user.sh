81b109addb6097ed3beb174fa2d5aae6f9ac3e00
Knu334 <who.is.fuu@gmail.com>
2024-11-26 13:00:27 +0900

    GithubActionsのUID確認

--- docker/create-user.sh
+++ docker/create-user.sh
@@ -4,4 +4,7 @@ GROUPID=${GROUP_ID}
 echo "Create User = $USERID. Group = $GROUPID"
 groupadd -g $GROUPID dockeruser
 useradd -m -s /bin/bash -u $USERID -g $GROUPID dockeruser
+whoami
+id
+ls -la /app/logs
 exec /usr/sbin/gosu $USERID "$@"
