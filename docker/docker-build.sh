149720b3fe19c700dfa9e41a272be422973cee3a
Knu334 <who.is.fuu@gmail.com>
2024-11-26 13:03:54 +0900

    デフォルトのユーザIDの修正

--- docker/docker-build.sh
+++ docker/docker-build.sh
@@ -3,8 +3,8 @@ if [ ! -e .env ]; then
     echo USERNAME= >> .env
     echo PASSWORD= >> .env
     echo NODE_ENV=production >> .env
-    echo USER_ID=1000 >> .env
-    echo GROUP_ID=1000 >> .env
+    echo USER_ID=1001 >> .env
+    echo GROUP_ID=1001 >> .env
     echo TZ=Asia/Tokyo >> .env
     echo LOG_LEVEL=INFO >> .env
 fi
